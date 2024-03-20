82b863e4678c8e60445762c01216d4396b342cab
Mark Davis <markd@trifoia.com>
2024-03-20 10:01:18 -0700

    using default images from MAUI

--- Oqtane.Server/wwwroot/Themes/Templates/External/Package/release.cmd
+++
@@ -1,3 +0,0 @@
-del "*.nupkg"
-"..\..\[RootFolder]\oqtane.package\nuget.exe" pack [Owner].Theme.[Theme].nuspec 
-XCOPY "*.nupkg" "..\..\[RootFolder]\Oqtane.Server\wwwroot\Themes\" /Y
--- Oqtane.Server/wwwroot/Themes/Templates/External/Package/release.sh
+++
@@ -1,2 +0,0 @@
-"..\..\oqtane.framework\oqtane.package\nuget.exe" pack [Owner].Theme.[Theme].nuspec 
-cp -f "*.nupkg" "..\..\oqtane.framework\Oqtane.Server\Packages\"
