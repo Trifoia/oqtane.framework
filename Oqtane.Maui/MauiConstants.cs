11f278dcf8c14565ac25e8cefc304dc6fc5842de
Mark Davis <markd@trifoia.com>
2024-04-02 21:06:47 -0700

    Update MauiConstants with production API URL and adjust TargetFramework version for Windows in csproj
    - Update MauiConstants with production API URL
    - Adjust TargetFramework version for Windows in csproj

--- Oqtane.Maui/MauiConstants.cs
+++ Oqtane.Maui/MauiConstants.cs
@@ -3,7 +3,8 @@ namespace Oqtane.Maui;
 public static class MauiConstants
 {
     // the API service url (used as fallback if not set in appsettings.json)
-    public static string ApiUrl = ""; 
+    public static string ApiUrl = "https://mindboba.azurewebsites.net/"; // for prod development (Oqtane.Server must be already running for MAUI client to connect)
+
     //public static string ApiUrl = "http://localhost:44357/"; // for local development (Oqtane.Server must be already running for MAUI client to connect)
     //public static string apiurl = "http://localhost:44357/sitename/"; // local microsite example
     //public static string apiurl = "https://www.dnfprojects.com/"; // for testing remote site
